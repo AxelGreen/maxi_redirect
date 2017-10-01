5ff70f7d2b6c28c2a83162c865acf9c9529681da
Axel <axelgreenkp@gmail.com>
2017-10-02 01:12:20 +0300

    installation update

--- config/apache.sh
+++ config/apache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# change index location
-sed -i "s/^.*DocumentRoot.*$/DocumentRoot \/var\/www/" /etc/apache2/sites-available/000-default.conf
+# TODO: improve replacement
+# change index location and add Directory tag
+sed -i "s/^.*DocumentRoot.*$/DocumentRoot \/var\/www\n<Directory \"\/var\/www\">\nAllowOverride All\n<\/Directory>/" /etc/apache2/sites-available/000-default.conf
 #enable rewrite module
 a2enmod rewrite
 # restart Apache to apply changes
