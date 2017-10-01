62783f27424de9b26ff7f9ede01d48f32ad59f42
Axel <axelgreenkp@gmail.com>
2017-10-02 01:02:12 +0300

    installation update

--- config/apache.sh
+++ config/apache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # change index location
 sed -i "s/^.*DocumentRoot.*$/DocumentRoot \/var\/www/" /etc/apache2/sites-available/000-default.conf
-
+#enable rewrite module
+a2enmod rewrite
 # restart Apache to apply changes
 /etc/init.d/apache2 restart
