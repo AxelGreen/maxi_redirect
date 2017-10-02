68e80e4d637f6e8d8830658e3d7ed46e34a23508
Axel Green <axelgreenkp@gmail.com>
2017-10-02 14:55:50 +0300

    installation update

--- scripts/bash/geoip_db_update.sh
+++ scripts/bash/geoip_db_update.sh
@@ -3,7 +3,7 @@ rm /usr/share/GeoIP/GeoIP.dat
 rm /usr/share/GeoIP/GeoIPv6.dat
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 gunzip GeoLiteCity.dat.gz
+mkdir /usr/share/GeoIP
 mv GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
-rm GeoLiteCity.dat.gz
 rm GeoLiteCity.dat
 /etc/init.d/apache2 restart
